1797d5064d2d3f4ff4f756cffab8ac00cf425fb0
wholzapfel <werner.holzapfel@trivento.nl>
2017-11-13 10:14:47 +0100

    package json

--- src/db/db.providers.ts
+++ src/db/db.providers.ts
@@ -13,6 +13,7 @@ export const dbProvider =
         useFactory: async () => await createConnection({
             type: 'postgres',
             host: process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
             port: parseInt(process.env.DB_PORT, 10),
             username: process.env.DB_USER,
             password: process.env.DB_PW,
