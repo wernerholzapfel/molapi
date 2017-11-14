44514772423ba773e8afcc8da4e79df1067d82d3
wholzapfel <werner.holzapfel@trivento.nl>
2017-11-14 20:58:53 +0100

    deelnemers call aangepast

--- src/deelnemers/deelnemers.controller.ts
+++ src/deelnemers/deelnemers.controller.ts
@@ -1,37 +1,81 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import * as slug from 'slug';
+import {Body, Controller, Get, Logger, Param, Post, Req, Res} from '@nestjs/common';
 
-import { Deelnemer } from './deelnemer.interface';
-import { CreateDeelnemerDto } from './create-deelnemer.dto';
-import { DeelnemersService } from './deelnemers.service';
+import {Deelnemer} from './deelnemer.interface';
+import {CreateDeelnemerDto} from './create-deelnemer.dto';
+import {DeelnemersService} from './deelnemers.service';
+import {ManagementClient} from 'auth0';
+import * as jwt_decode from 'jwt-decode';
+import 'dotenv/config';
+
+const auth0Token = process.env.AUTH0_TOKEN;
+const auth0Domain = process.env.AUTH0_DOMAIN;
 
 @Controller('deelnemers')
 export class DeelnemersController {
-
-  constructor(
-    private readonly deelnemersService: DeelnemersService,
-  ) { }
-
-  @Get()
-  async findAll(): Promise<Deelnemer[]> {
-    return this.deelnemersService.findAll();
-  }
-
-  @Post()
-  async create( @Body() createDeelnemerDto: CreateDeelnemerDto) {
-    const newEntry = Object.assign({}, createDeelnemerDto, {
+    private readonly logger = new Logger('deelnemersController', true);
+    private management = new ManagementClient({
+        domain: auth0Domain,
+        token: auth0Token,
     });
-    await this.deelnemersService.create(newEntry);
-  }
 
-  @Delete(':deelnemerId')
-  delete( @Param('deelnemerId') deelnemerId) {
-    return this.deelnemersService.deleteOne(deelnemerId);
-  }
+    constructor(private readonly deelnemersService: DeelnemersService) {
+    }
 
-  @Get(':deelnemerId/voorspellingen')
-  findEntriesByCategory( @Param('deelnemerId') deelnemerId): Promise<Deelnemer> {
-    return this.deelnemersService.findVoorspellingen(deelnemerId);
-  }
+    @Get()
+    async findAll(): Promise<Deelnemer[]> {
+        return this.deelnemersService.findAll();
+    }
 
+    @Post()
+    async create(@Body() createDeelnemerDto: CreateDeelnemerDto) {
+        const newEntry = Object.assign({}, createDeelnemerDto, {});
+        await this.deelnemersService.create(newEntry);
+    }
+
+    @Get(':deelnemerId/voorspellingen')
+    findEntriesByCategory(@Param('deelnemerId') deelnemerId): Promise<Deelnemer> {
+        return this.deelnemersService.findVoorspellingen(deelnemerId);
+    }
+
+    @Get('loggedIn')
+    async findLoggedInDeelnemer(@Req() req, @Res() res) {
+        const extractedToken = this.getToken(req.headers);
+        if (extractedToken) {
+            // async.waterfall([
+            //     callback => {
+            this.logger.log('start decoding');
+            const decoded: any = jwt_decode(extractedToken);
+            this.logger.log(decoded.sub);
+            // callback(null, decoded);
+            // },
+            // (decodedUser, callback) => {
+            this.management.getUser({
+                id: decoded.sub,
+                // if (!user.email_verified) return res.status(200).json('Om wijzigingen door te kunnen voeren moet je eerst je mail verifieren. Kijk in je mailbox voor meer informatie.');
+                // callback(null, user);
+                // });
+                // },
+                // (user, callback) => {
+                //     return this.deelnemersService.findLoggedInDeelnemer('auth0|57b326157d6c918d6a161d77');
+                // },
+                // ]);
+            }).then(async user => {
+                this.deelnemersService.findLoggedInDeelnemer(user.user_id,res);
+            });
+        }
+    }
+
+    // todo remove upperclass
+    getToken = headers => {
+        if (headers && headers.authorization) {
+            const parted = headers.authorization.split(' ');
+            if (parted.length === 2) {
+                return parted[1];
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    };
 }
--- src/deelnemers/deelnemers.service.ts
+++ src/deelnemers/deelnemers.service.ts
@@ -1,24 +1,26 @@
-import {Component, Inject} from '@nestjs/common';
+import {Component, HttpStatus, Inject, Logger} from '@nestjs/common';
 import {Repository} from 'typeorm';
 
 import {Deelnemer} from './deelnemer.interface';
 
 @Component()
 export class DeelnemersService {
+    private readonly logger = new Logger('deelnemerService', true);
+
     constructor(@Inject('DeelnemerRepositoryToken') private readonly deelnemerRepository: Repository<Deelnemer>) {
     }
 
     async findAll(): Promise<Deelnemer[]> {
         try {
             return await this.deelnemerRepository.find(
-                // {
-                //     join: {
-                //         alias: 'deelnemer',
-                //         leftJoinAndSelect: {
-                //             voorspellingen: 'deelnemer.voorspellingen',
-                //         },
-                //     },
-                // },
+                {
+                    join: {
+                        alias: 'deelnemer',
+                        leftJoinAndSelect: {
+                            voorspellingen: 'deelnemer.voorspellingen',
+                        },
+                    },
+                },
             );
         } catch (err) {
             return err;
@@ -33,17 +35,27 @@ export class DeelnemersService {
         }
     }
 
-    async deleteOne(deelnemerId: string) {
+    async findVoorspellingen(deelnemerId: string) {
         try {
-            return await this.deelnemerRepository.removeById(deelnemerId);
+            this.logger.log('vind voorspelling van deelnemer: ' + deelnemerId);
+            return await this.deelnemerRepository.findOneById(deelnemerId, {
+                join: {
+                    alias: 'deelnemer',
+                    leftJoinAndSelect: {
+                        voorspellingen: 'deelnemer.voorspellingen',
+                    },
+                },
+            });
         } catch (err) {
             return err;
         }
     }
 
-    async findVoorspellingen(deelnemerId: string) {
+    async findLoggedInDeelnemer(user_id: string, res) {
         try {
-            return await this.deelnemerRepository.findOneById(deelnemerId);
+            await this.deelnemerRepository.findOne({where: {auth0Identifier: user_id}}).then(response => {
+                return res.status(HttpStatus.ACCEPTED).json(response).send();
+            });
         } catch (err) {
             return err;
         }
