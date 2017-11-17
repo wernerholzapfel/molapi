d4bb107b3f65ce3b8d20b132fec464b50fa6fb7b
wholzapfel <werner.holzapfel@trivento.nl>
2017-11-18 00:04:43 +0100

    quizresultaten worden bijgewerkt als mol bekend is.

--- src/kandidaten/kandidaten.service.ts
+++ src/kandidaten/kandidaten.service.ts
@@ -4,6 +4,8 @@ import {Kandidaat} from './kandidaat.entity';
 import {Voorspelling} from '../voorspellingen/voorspelling.entity';
 import {Afleveringpunten} from '../afleveringpunten/afleveringpunt.entity';
 import {Deelnemer} from '../deelnemers/deelnemer.entity';
+import {Quizantwoord} from '../quizantwoorden/quizantwoord.entity';
+import {Quizresultaat} from '../quizresultaten/quizresultaat.entity';
 
 @Component()
 export class KandidatenService {
@@ -11,15 +13,36 @@ export class KandidatenService {
 
     molPunten: number = 20;
     winnaarPunten: number = 5;
+    vragenPunten: number = 10;
 
     constructor(@Inject('kandidaatRepositoryToken') private readonly kandidaatRepository: Repository<Kandidaat>) {
     }
 
-    async findAll(): Promise<Kandidaat[]> {
-        try {
-            return await this.kandidaatRepository.find();
-        } catch (err) {
-            return err;
+    async findAll(): Promise<any[]> {
+        const kandidaten = await getRepository(Kandidaat).find();
+        const mol = kandidaten.filter(kandidaat => {
+            return kandidaat.mol;
+        });
+        if (mol.length > 0) {
+            this.logger.log('dit is de mol: ' + mol[0].display_name);
+            const answers = await getRepository(Quizantwoord).find(
+                {
+                    join: {
+                        alias: 'quizantwoord',
+                        leftJoinAndSelect: {
+                            kandidaten: 'quizantwoord.kandidaten',
+                        },
+                    },
+                },
+            );
+            const correctAnswers = answers.filter(answer => {
+                return answer.kandidaten.find(
+                    kandidaat => {
+                        return kandidaat.id === mol[0].id;
+                    });
+            });
+            this.logger.log('aantal vragen met mol: ' + correctAnswers.length);
+            return correctAnswers;
         }
     }
 
@@ -46,9 +69,6 @@ export class KandidatenService {
             return voorspelling.afvaller.id === kandidaat.id && voorspelling.aflevering === kandidaat.elimination_round;
         });
         await deelnemers.forEach(async deelnemer => {
-            // afvallerpunten = await this.determineAfvallerPunten(deelnemer, voorspellingen, kandidaat);
-            // molpunten = await this.determineMolPunten();
-            // winnaarpunten = await this.determineWinnaarPunten();
             await getRepository(Afleveringpunten).save({
                 aflevering: kandidaat.elimination_round,
                 afvallerpunten: await this.determineAfvallerPunten(deelnemer, voorspellingen, kandidaat),
@@ -58,6 +78,48 @@ export class KandidatenService {
                 voorspelling: {id: await this.determineVoorspellingId(deelnemer, voorspellingen, kandidaat)},
             });
         });
+        await this.updateQuizResultaten();
+    }
+
+    async updateQuizResultaten() {
+        const kandidaten = await getRepository(Kandidaat).find();
+        const mol = kandidaten.find(kandidaat => {
+            return kandidaat.mol;
+        });
+        if (mol) {
+            this.logger.log('dit is de mol: ' + mol.display_name);
+            const answers = await getRepository(Quizantwoord).find(
+                {
+                    join: {
+                        alias: 'quizantwoord',
+                        leftJoinAndSelect: {
+                            kandidaten: 'quizantwoord.kandidaten',
+                        },
+                    },
+                },
+            );
+            const correctAnswers: Quizantwoord[] = answers.filter(answer => {
+                return answer.kandidaten.find(
+                    kandidaat => {
+                        return kandidaat.id === mol.id;
+                    });
+            });
+            this.logger.log('aantal vragen met mol: ' + correctAnswers.length);
+
+            const quizresultaten: Quizresultaat[] = await getRepository(Quizresultaat).find();
+            await quizresultaten.forEach(async quizresultaat => {
+                if (correctAnswers.find(correctAnswer => {
+                        this.logger.log('correctantwoord ' + correctAnswer.id + '-' + quizresultaat.antwoord.id);
+                        return correctAnswer.id === quizresultaat.antwoord.id;
+                    })) {
+                    quizresultaat.punten = this.vragenPunten;
+                }
+                else {
+                    quizresultaat.punten = 0;
+                }
+                await getRepository(Quizresultaat).save(quizresultaat);
+            });
+        }
     }
 
     determineAfvallerPunten(deelnemer: Deelnemer, voorspellingen: Voorspelling[], kandidaat: Kandidaat) {
@@ -89,9 +151,9 @@ export class KandidatenService {
     }
 
     determineVoorspellingId(deelnemer: Deelnemer, voorspellingen: Voorspelling[], kandidaat: Kandidaat) {
-            return voorspellingen.find(voorspelling => {
-                return voorspelling.deelnemer.id === deelnemer.id &&
-                    voorspelling.aflevering === kandidaat.elimination_round;
-            }).id;
+        return voorspellingen.find(voorspelling => {
+            return voorspelling.deelnemer.id === deelnemer.id &&
+                voorspelling.aflevering === kandidaat.elimination_round;
+        }).id;
     }
 }
