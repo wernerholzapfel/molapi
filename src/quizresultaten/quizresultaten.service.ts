a1da868a2b77759f9b087306dfd0d7d75aaddcf8
wholzapfel <werner.holzapfel@trivento.nl>
2017-11-26 21:17:44 +0100

    quizresultaten ophalen uitgebrei

--- src/quizresultaten/quizresultaten.service.ts
+++ src/quizresultaten/quizresultaten.service.ts
@@ -9,24 +9,37 @@ import * as _ from 'lodash';
 @Component()
 export class QuizresultatenService {
     private readonly logger = new Logger('quizresultatenService', true);
+    aflevering: number;
 
     constructor(@Inject('QuizresultatenRepositoryToken') private readonly quizresultaatRepository: Repository<Quizresultaat>) {
     }
 
     async findAll(auth0Identifier: string): Promise<Quizresultaat[]> {
-        const afleveringen = await getRepository(Aflevering).find({where: {uitgezonden: false}}).catch((err) => {
+        const afleveringen = await getRepository(Aflevering).find().catch((err) => {
             throw new HttpException({message: err.message, statusCode: HttpStatus.BAD_REQUEST}, HttpStatus.BAD_REQUEST);
         });
-        const aflevering = _.minBy(afleveringen, 'aflevering').aflevering;
-        this.logger.log('dit is de huidige aflevering: ' + aflevering);
+        const afleveringObject = afleveringen.find(item => {
+            return item.laatseAflevering;
+        });
+        if (afleveringObject) {
+            this.aflevering = afleveringObject.aflevering - 1;
+        }
+        else {
 
+            const uitgezondenAfleveringen = afleveringen.filter(item => {
+                return !item.uitgezonden;
+            });
+            this.aflevering = _.minBy(uitgezondenAfleveringen, 'aflevering').aflevering;
+        }
+
+        this.logger.log('dit is de huidige aflevering: ' + this.aflevering);
         const deelnemer = await getRepository(Deelnemer).findOne({where: {auth0Identifier}});
 
         return await getRepository(Quizresultaat)
             .createQueryBuilder('resultaat')
             .leftJoinAndSelect('resultaat.vraag', 'vraag')
             .leftJoinAndSelect('resultaat.antwoord', 'antwoord')
-            .where('resultaat.aflevering = :aflevering', {aflevering})
+            .where('resultaat.aflevering = :aflevering', {aflevering: this.aflevering})
             .andWhere('resultaat.deelnemer = :deelnemerId', {deelnemerId: deelnemer.id})
             .getMany()
             .catch((err) => {
