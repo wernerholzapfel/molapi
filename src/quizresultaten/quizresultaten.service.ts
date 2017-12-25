0be265c03e94b4a78a763a4d876118014aeb11cf
wholzapfel <werner.holzapfel@trivento.nl>
2017-12-25 17:38:51 +0100

    getquizresultaten van laatste uitgezonden aflevering

--- src/quizresultaten/quizresultaten.service.ts
+++ src/quizresultaten/quizresultaten.service.ts
@@ -9,7 +9,7 @@ import * as _ from 'lodash';
 @Component()
 export class QuizresultatenService {
     private readonly logger = new Logger('quizresultatenService', true);
-    afleveringWithLatestTest: number;
+    latestUitgezondenAflevering: number;
 
     constructor(@Inject('QuizresultatenRepositoryToken') private readonly quizresultaatRepository: Repository<Quizresultaat>) {
     }
@@ -18,29 +18,31 @@ export class QuizresultatenService {
         const afleveringen = await getRepository(Aflevering).find().catch((err) => {
             throw new HttpException({message: err.message, statusCode: HttpStatus.BAD_REQUEST}, HttpStatus.BAD_REQUEST);
         });
-        const afleveringenWithTestList = afleveringen.filter(item => {
-            return item.hasTest;
+        const afleveringenUitgezondenList = afleveringen.filter(item => {
+            return item.uitgezonden;
         });
 
-        if (afleveringenWithTestList.length > 0) {
-            this.afleveringWithLatestTest = _.maxBy(afleveringenWithTestList, 'aflevering').aflevering;
-        }
-        else {
+        this.latestUitgezondenAflevering = _.maxBy(afleveringenUitgezondenList, 'aflevering').aflevering;
 
-            const uitgezondenAfleveringen = afleveringen.filter(item => {
-                return item.uitgezonden;
-            });
-            this.afleveringWithLatestTest = _.maxBy(uitgezondenAfleveringen, 'aflevering').aflevering;
-        }
+        // if (laatsteUitgezondenAflevering.hasTest) {
+        //     this.afleveringWithLatestTest = _.maxBy(afleveringenUitgezondenList, 'aflevering').aflevering;
+        // }
+        // else {
+        //
+        //     const uitgezondenAfleveringen = afleveringen.filter(item => {
+        //         return item.uitgezonden;
+        //     });
+        //     this.afleveringWithLatestTest = _.maxBy(uitgezondenAfleveringen, 'aflevering').aflevering;
+        // }
 
-        this.logger.log('dit is de huidige aflevering: ' + this.afleveringWithLatestTest);
+        this.logger.log('dit is de huidige aflevering: ' + this.latestUitgezondenAflevering);
         const deelnemer = await getRepository(Deelnemer).findOne({where: {auth0Identifier}});
 
         return await getRepository(Quizresultaat)
             .createQueryBuilder('resultaat')
             .leftJoinAndSelect('resultaat.vraag', 'vraag')
             .leftJoinAndSelect('resultaat.antwoord', 'antwoord')
-            .where('resultaat.aflevering = :aflevering', {aflevering: this.afleveringWithLatestTest})
+            .where('resultaat.aflevering = :aflevering', {aflevering: this.latestUitgezondenAflevering})
             .andWhere('resultaat.deelnemer = :deelnemerId', {deelnemerId: deelnemer.id})
             .getMany()
             .catch((err) => {
