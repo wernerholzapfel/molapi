26a4e9c4301744c4f788249c847a8c9257436b27
wholzapfel <werner.holzapfel@trivento.nl>
2018-12-23 10:40:17 +0100

    afronding statistieken + updatedatetime bij acties

--- src/acties/create-actie.dto.ts
+++ src/acties/create-actie.dto.ts
@@ -2,6 +2,7 @@ import {IsInt} from 'class-validator';
 
 export class CreateActiesDto {
     readonly id: string;
+    readonly updatedDate: Date;
 
     @IsInt() readonly voorspellingaflevering: number;
     @IsInt() readonly testaflevering: number;
